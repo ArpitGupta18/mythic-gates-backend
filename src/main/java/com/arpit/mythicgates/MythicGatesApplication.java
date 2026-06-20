ad2b8661c84c1b7372650bc5099de378086fbe18
Arpit Gupta <gupta.14arpit@gmail.com>
2026-06-20 16:27:43 +0545

    init: project initialization

---
+++ src/main/java/com/arpit/mythicgates/MythicGatesApplication.java
@@ -0,0 +1,13 @@
+package com.arpit.mythicgates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythicGatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MythicGatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arpit/mythicgates/MythicGatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpit.mythicgates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythicGatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
