77acf02d17a7501accfaa781aeed101677e18f68
ArpitGupta18 <gupta.14arpit@gmail.com>
2026-06-25 15:36:23 +0545

    fix: cors enabled; allow credentials

--- src/main/java/com/arpit/mythicgates/config/SecurityConfig.java
+++ src/main/java/com/arpit/mythicgates/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.arpit.mythicgates.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,6 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .authenticationProvider(applicationConfig.authenticationProvider())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
